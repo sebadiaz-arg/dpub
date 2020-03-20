d6c1c7dbaf8081134e4d1b44ff2b374a72fa05c7
telefonica <telefonica@Clementina.local>
2020-03-20 12:04:32 -0300

    Moving changes in dpub-2.py to dpub.py

--- dpub.py
+++ dpub.py
@@ -6,10 +6,16 @@
 # "========================" before a Request
 # "------------------------" before a Response
 # "************************" at the end of file
+''' Notes: 
+-The data from online sheet is read from second row. The first row is for the title.
+-The test cases names must be exactly the same in Postman collection than in the online sheet.
+-The online sheet must contain three columns in this order: test cases, requests and responses.
+'''
 
 import os
 import pickle
 import sys
+import string
 from argparse import ArgumentParser
 
 from google.auth.transport.requests import Request
@@ -24,6 +30,13 @@ _EXCEL_2007_MIME_TYPE = 'application/vnd.openxmlformats-officedocument.spreadshe
 
 _SHEETS_SERVICE = 'sheets'
 _SHEETS_SERVICE_VERSION = 'v4'
+'''It's the end of the range, from A1, where we'll get the online sheet to be compared locally after send the updates. Increase if you have a bigger data range in your test plan sheet.'''
+_END_RANGE = ':B60'
+_FIRST_ROW_FREE_AFTER_TITLE = 2 #ROW 1 has the title
+_COL_WITH_TEST_CASES = "A"
+FALSE=0
+TRUE=1
+
 
 class PublishToDriveError(Exception):
     pass
@@ -68,7 +81,7 @@ def _validate_cell_ref(cell_ref):
             'Only single cells are valid so far: {} is wrong'.format(cell_ref))
 
 
-def _write(document, cell, content, credentials):
+def _write(document, cell, values, credentials):
     '''Writes certain content to a range in a spreadsheet'''
     service = build(_SHEETS_SERVICE, _SHEETS_SERVICE_VERSION,
                     credentials=credentials)
@@ -78,9 +91,7 @@ def _write(document, cell, content, credentials):
     body = {
         'range': cell,
         'majorDimension': 'COLUMNS',
-        'values': [
-            content[0],content[1]
-        ]
+        'values': values ### AQUI DEBO MODIFCAR PARA QUE SE CORRAN LAS COLUMNAS EN DONDE UPDATEAR EN FUNCION DE LA COLUMNA INGRESADA EN POR ARGMENTO.
     }
 
     service.spreadsheets().values().update(
@@ -93,7 +104,7 @@ def _parse_args():
         'spreadsheet', help='The spreadsheet id in Google Drive where publishing')
     parser.add_argument('sheet', help='The sheet where publishing')
     parser.add_argument('cell', help='The first cell where begin publishing, i.e. \'C2\'')
-    parser.add_argument('test', help='Column letter used for test names, i.e. \'B\'')
+    #parser.add_argument('tests', help='Column letter used for test names, i.e. \'B\'')
     parser.add_argument('-c', '--credentials', help='path to the credentials file',
                         default='./{}'.format(_CREDS_FILE))
     parser.add_argument('-t', '--token', help='path to the access token file',
@@ -107,38 +118,145 @@ def _parse_args():
     return args
 
 
-def insertOneByOne(listToInsert, listInSheet):
-    for listToInsert[0] in line:
-        pos=position de line en Lista
-        post lista_a_insertar[1] en posicion correspondiente
+def _getSheet(document, range, credentials):
+    table=[]
+    service = build(_SHEETS_SERVICE, _SHEETS_SERVICE_VERSION,
+                    credentials=credentials)
+    range = "{}{}".format(range,_END_RANGE)
+    getRequest = service.spreadsheets().values().get(spreadsheetId=document, range=range, valueRenderOption='FORMATTED_VALUE')
+    table = getRequest.execute()
+    return table
+
+### CORREGIR PARA QUE BUSQUE FRASES COMPLETAS, y no solo palabras
+def _find_element_in_list(element, list_element):
+    listing=list_element
+    try:
+        indexPos = listing.index(element)
+        return indexPos
+    except ValueError:
+        return None
+
+
+def prepareTable(content, onlineSheetRowList, sheetLenght, row):
+    ''' We looking for a test case name into the test cases names from the online sheet. 
+    Then we get its position into the sheet. '''
+    indexPos=None
+    key=0
+    if sheetLenght > 0:
+        newValuesPos=sheetLenght + 2                # It'll indicate the first empty row at the end of the data in the online sheet
+    else:
+        newValuesPos=_FIRST_ROW_FREE_AFTER_TITLE    # If the sheet is empty, we begin writing in the second row, because the first one is the title
+
+    onlineSheetRowDict={}
+    value0=content[0] # test case name
+    value1=content[1] # request
+    value2=content[2] # response
+    for testcase in value0:
+      if sheetLenght > 0:   # If online sheet is empty we don't need check if the test case was executed. It wasn't there before.
+        indexPos=_find_element_in_list(testcase, onlineSheetRowList)
+        #print("PARA FINAL, indexPos:", indexPos)
+
+      dataInDict=[value0[key],value1[key],value2[key]]
+      key += 1
+      #print("dataInDict:", dataInDict)
+      #print(dataInDict)      
+      # If the test case name is not in the Online sheet, we'll have indexPos=None
+      if indexPos != None:
+        indexPosStr=str(indexPos)
+        indexPos=int(indexPosStr)
+        indexPos += row
+        ''' onlineSheetRowDict will have in each position a list with three values: test case name, request and response. 
+        The order number is the row position. '''
+        onlineSheetRowDict.update({ indexPos: dataInDict })
+      elif indexPos == None:   # indexPos only will be None when it is a test case that wasn't in the online sheet
+        newValuesPosStr=str(newValuesPos)
+        newValuesPos=int(newValuesPosStr)
+        onlineSheetRowDict.update({ newValuesPos: dataInDict })
+        #print("es NONE!:", onlineSheetRowDict)
+        newValuesPos += 1
+    
+    ''' onlineSheetRowDict has each row position with an array with test case name, request and response. '''
+    #print(onlineSheetRowDict)
+    return onlineSheetRowDict
+
+
+def createLastTable(onlineSheetRowDict, newValuesPos):
+    ''' onlineSheetRowDict has each row position with an array with test case name, request and response. '''
+    value=[]
+    values=[]
+    empty=[]
+    pairDataA=[]
+    pairDataB=[]
+    pairDataC=[]
+    add_test_cases_column=FALSE
+    ''' We make a list, in same order than the data in online sheet, with the new requests and responses. 
+    The rows that shouldn't be modified will have None content to be skipped. '''
+    if newValuesPos == 0:
+        newValuesPos=len(onlineSheetRowDict)
+
+### REVISAR ESTE RANGO
+    for i in range(_FIRST_ROW_FREE_AFTER_TITLE,100):
+        if i in onlineSheetRowDict:
+            value=onlineSheetRowDict[i]
+            #Inserting test case column if the sheet is empty
+            #Inserting request and response
+            pairDataA.append(value[0])
+            pairDataB.append(value[1])
+            pairDataC.append(value[2])
+        else:
+            pairDataA.append(None)  #Inserting test case column if the sheet is empty
+            pairDataB.append(None)
+            pairDataC.append(None)
+
+    values.append(pairDataA)
+    values.append(pairDataB)
+    values.append(pairDataC)
+    return values
 
 
 def main():
     args = _parse_args()
     creds = _lookup_credentials(args.credentials, args.token)
-#row is to follow the row position where we'll insert text into a spreadsheet. 
-#The first insertion will be on row=2, because the first row in the spreadsheet has the titles.
+    #row is to follow the row position where we'll insert text into a spreadsheet. 
+    #The first insertion will be on row=2, because the first row in the spreadsheet has the titles.
     column=args.cell[0]  #Column letter
     row=int(args.cell[1:])   #Row number
     control=""
+    testcase=""
+    onlineSheet=[]
+    rowAndTest=[]
+    onlineSheetRow=[]
+    onlineSheetRowList=[]
+    listTemp=[]
+    onlineSheetRowDict={}
     arresponse=[]
     arrequest=[]
+    testjoin=[]
     reqjoin=[]
     resjoin=[]
     content=[]   #An arrays of arrays where content[0] will have the Requests arrays and content[1] the Responses arrays.
+    contentTemp=[]
+    listNumbers=[]
 
     #Preparing the info to indicate where begin the location to paste the data into the spreadsheet.
-    reqrange="\'{}\'!{}{}".format(args.sheet,column,row)
+    readrange="\'{}\'!{}{}".format(args.sheet,column,row)
+    writerange="\'{}\'!{}{}".format(args.sheet,column,row)
+    # An integer list since 0 to 100 to use after
+    # listNumbers=list(range(0,100))
     #We'll read the stdin line by line, putting the Response in one array and the Request in another one. 
     for line in sys.stdin:
-     if "========================" not in line and "------------------------" not in line and "************************" not in line:
-       if control == "request":
+     if "========================" not in line and "------------------------" not in line \
+       and "************************" not in line and len(line) > 1:  # We check line with content, not empty line
+       if "Test case: " in line:
+        testcase=(line[11:-2])
+        testjoin.append(testcase)
+       elif control == "request":
         arrequest.append(line)
        elif control == "response":   
         arresponse.append(line)
 
      if "************************" in line or "========================" in line:
-      control="request"   #We've found a Request
+      control="request"   #We mark to indicate that we've found a Request
       if arrequest != []:
        #We prepare the position to insert the Request and Response into the spreadsheet.
        #After getting a Request and a Response, line by line into an array, we join the content to get a string for each one.
@@ -151,9 +269,43 @@ def main():
      elif "------------------------" in line:
       control="response"   #We've found a Response
 
-    content.append(reqjoin)   #Inserting the array with the requests in content[0]
-    content.append(resjoin)   #Inserting the array with the responses in content[1]
-    _write(args.spreadsheet, reqrange, content, creds)   #Inserting Request into spreadsheet
+    content.append(testjoin)  #Inserting the array with the test case names in content[0]
+    content.append(reqjoin)   #Inserting the array with the requests in content[1]
+    content.append(resjoin)   #Inserting the array with the responses in content[2]
+
+    '''We get the data from the online sheet, at least from the column with test case names'''
+    onlineSheet=_getSheet(args.spreadsheet, readrange, creds)   #Inserting Requests and Responses into spreadsheet
+    #print (onlineSheet)
+    '''Checking if the online sheet has data or is empty '''
+    if 'values' in onlineSheet:
+      listTemp=onlineSheet['values']
+    else:
+      onlineSheet.update({ 'values': [] })
+      listTemp=[]
+
+    ''' Moving the test cases names we've got from online sheet towards onlineSheetRowList[] '''
+    ''' If the online sheet is empty, then listTemp will be empty so onlineSheetRowList will be empty as well. '''
+    ''' listTemp has the test cases we've got from online sheet '''
+    key=0
+    for i in listTemp[0:]:        
+        if len(i) > 0:
+            onlineSheetRowList.append(i[0])
+        else: 
+            onlineSheetRowList.append([])  ### CHECK THIS
+
+        key += 1
+
+    '''Preparing a dictionary with number row order and test case name, and a list with the test case names '''
+    sheetLenght=len(onlineSheetRowList)
+    onlineSheetRowDict=prepareTable(content, onlineSheetRowList, sheetLenght, row)
+    #print(onlineSheetRowDict)
+    #print("")
+    newValuesPos=sheetLenght
+    lastTableToUpload=createLastTable(onlineSheetRowDict, newValuesPos)
+    #print("Lasttable!: ", lastTableToUpload)
+    _write(args.spreadsheet, writerange, lastTableToUpload, creds)   #Inserting Requests and Responses into spreadsheet
+
 
 if __name__ == "__main__":
     main()
+ 
